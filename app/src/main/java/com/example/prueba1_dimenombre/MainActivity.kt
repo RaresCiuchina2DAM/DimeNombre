b8c2aabaca4e240e2d7f68d70ce2b87ca84ea129
RaresCiuchina2DAM <rarciu763@alu.centroafuera.es>
2022-10-27 13:30:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/prueba1_dimenombre/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.prueba1_dimenombre
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    //Declaración de variables
+    private lateinit var txtNombre : EditText
+    private lateinit var btnAceptar : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Obtenemos una referencia a los controles de la interfaz
+        txtNombre = findViewById(R.id.Texto)
+        btnAceptar = findViewById(R.id.BotonAceptar)
+
+
+        //Implementamos el evento click del botón
+        btnAceptar.setOnClickListener(){
+            if (validar()){
+                val intent = Intent(this@MainActivity, SaludoActivity::class.java)
+                intent.putExtra("nombre", txtNombre.text.toString())
+                startActivity(intent)
+
+            }
+        }
+    }
+    //Validamos que el nombre no esté vacío
+    fun validar (): Boolean {
+        return if (txtNombre.text.toString().isEmpty()){
+            txtNombre.error = "Campo obligatorio"
+            false
+        }else true
+    }
+
+
+}
+
+
+
+
---
+++ app/src/main/java/com/example/prueba1_dimenombre/SaludoActivity.kt
@@ -0,0 +1,29 @@
+package com.example.prueba1_dimenombre
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SaludoActivity : AppCompatActivity() {
+
+    private lateinit var txtSaludo : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saludo)
+
+
+        //Obtenemos una referencia a los controles de la interfaz
+        txtSaludo = findViewById(R.id.MostrarSaludo)
+
+        //Recuperamos la información pasada en el intent
+        val nombre = intent.getStringExtra("nombre")
+
+
+
+        //Mostramos el saludo
+        txtSaludo.text = "Hola $nombre"
+
+
+    }
+}
